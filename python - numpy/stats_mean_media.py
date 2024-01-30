f5578401b9b9a9be4d408a02a4a299e301d969e9
vamsiyenumula <vamsiyenumula4@gmail.com>
2024-01-30 11:02:52 +0530

    stats_mean_media.py

---
+++ python - numpy/stats_mean_media.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+mean = np.mean(arr)
+median = np.median(arr)
+std_dev = np.std(arr)
+print(f'Mean: {mean}, Median: {median}, Standard Deviation: {std_dev}')
